560b1ebfb78a8542c73fcb42e500f47aeceee802
Juju Nakarmi <152660757+Jujunakarmi@users.noreply.github.com>
2024-05-09 19:18:01 -0400

    added get post put del routes

--- controllers/api/petsRoutes.js
+++ controllers/api/petsRoutes.js
@@ -1,7 +1,22 @@
 const router = require('express').Router();
 
+// Route api/projects
 router.get('/', (req,res) => {
 res.send('petsroute')
-})
+});
+
+
+router.post('/', (req,res) => {
+
+});
+
+
+router.put('/', (req,res) => {
+
+});
+
+router.delete('/', (req,res) => {
+
+});
 
 module.exports = router;
