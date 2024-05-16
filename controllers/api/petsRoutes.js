c7b317529c51882b943f3e6680e125bf15f2a8cf
ijmckoy <Ikeman200@gmail.com>
2024-05-16 19:12:05 -0400

    update pets routs

--- controllers/api/petsRoutes.js
+++ controllers/api/petsRoutes.js
@@ -10,7 +10,18 @@ router.get('/', (req,res) => {
 });
 
 
-router.post('/', (req,res) => {
+router.post('/',async (req,res) => {
+
+    try {
+        const newPet = await Pet.create({
+          ...req.body,
+          user_id: req.session.user_id,
+        });
+    
+        res.status(200).json(newProject);
+      } catch (err) {
+        res.status(400).json(err);
+      }
 
 });
 
